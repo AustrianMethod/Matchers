3ff88bb0ea9f606285f3637440c5b4a7820e6799
AustrianMethod <ivanov.nv.spb@gmail.com>
2024-10-22 19:23:00 +0300

    added husky

--- test.js
+++ test.js
@@ -1,16 +1,16 @@
-import getState from './src/app.js';
+import getState from './src/app';
 
 test('getState', () => {
   const arr = [
-    {name: 'мечник', health: 10},
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
-  ]
-  
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+
   const result = getState(arr);
   expect(result).toEqual([
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
-    {name: 'мечник', health: 10},
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
   ]);
 });
