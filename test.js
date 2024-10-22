2ff37eb0c9e2b654b42ee26529beab0ef5eb2a76
AustrianMethod <ivanov.nv.spb@gmail.com>
2024-10-22 15:54:29 +0300

    done

---
+++ test.js
@@ -0,0 +1,16 @@
+import getState from './src/app.js';
+
+test('getState', () => {
+  const arr = [
+    {name: 'мечник', health: 10},
+    {name: 'маг', health: 100},
+    {name: 'лучник', health: 80},
+  ]
+  
+  const result = getState(arr);
+  expect(result).toEqual([
+    {name: 'маг', health: 100},
+    {name: 'лучник', health: 80},
+    {name: 'мечник', health: 10},
+  ]);
+});
